6a10e6d1904e3295f8f922a5e70c111555e22f47
amillena <amillena@yahoo.com>
2017-05-01 18:25:56 -0700

    initial upload

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burger (burger_name, devoured) VALUES ('Cheese Burger', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Big Mac', true);
+
